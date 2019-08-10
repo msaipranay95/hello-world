13b468619fd2bdcb84b36eaca878309c553fa0e6
pranay <53944387+msaipranay95@users.noreply.github.com>
2019-08-10 18:20:53 +0530

    Create prime.py
    wanna excitute prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+i = 2
+x = int(input('please enter the number'))
+while (i < x):
+    if (x % i == 0):
+        print('its not a prime number')
+        break
+    i = i + 1
+else:
+    print('prime number')
+    
+print("this 1 is frm github and prime exicutation ")
